06c036b42b9d35589f7d00ea441f71f455de9336
Erich Blume <blume.erich@gmail.com>
2013-04-07 00:07:59 -0700

    Added the first actual test

---
+++ tests/map_test.py
@@ -0,0 +1,18 @@
+"Tests for presto.map"
+
+import unittest as ut
+
+from presto.map.system import System
+
+class TestMap(ut.TestCase):
+
+    def test_map(self):
+        "Quick test on some map data."
+        stacmon = System.by_name("Stacmon")
+        self.assertTrue(stacmon)
+        self.assertEqual(len(list(stacmon.neighbors())), 5)
+        self.assertTrue("Ostingele" in {n.name for n in stacmon.neighbors()})
+
+
+if __name__ == '__main__':
+    unittest.main()
